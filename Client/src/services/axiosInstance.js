de2d6c3164b1cffec089a640fcb79d3fb29158ab
mani901 <abrehman8491@gmail.com>
2025-08-13 11:02:59 +0500

    setup env for frontend

--- Client/src/services/axiosInstance.js
+++ Client/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const apiUrl = import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
-  baseURL: "https://mern-jobportal-production.up.railway.app",
+  baseURL: apiUrl,
   withCredentials: true,
 });
 
